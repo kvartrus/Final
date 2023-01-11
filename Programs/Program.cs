eb73624dc52640770a89a4ec2d0718265d672569
«Kvartal» <pavsharv@yandex.ru>
2023-01-12 01:10:19 +0300

    Создана программа решения

---
+++ Programs/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+string [] startingArray = new string[] {"hello", "2", "world", ";-)"};
+string[] resultArray = new string [0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
+    }
+}
+
+Console.WriteLine($"[{String.Join("; ", resultArray)}]");
